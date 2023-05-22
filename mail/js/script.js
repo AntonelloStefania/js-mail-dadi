23c53820b347ea5b78730e4afd7e9ac129216c2a
AntonelloStefania <antonelloste94@gmail.com>
2023-05-22 18:59:22 +0200

    [add] stile e contenuto esercizio mail

---
+++ mail/js/script.js
@@ -0,0 +1,21 @@
+let mails = [ 'pincopallino@msn.com','mariafranca@msn.com', 'giangiorgio@msn.com', 'alfredo@msn.com', 'mariella@msn.com', 'riccardinofuffo@msn.com', 'ferrogigio@msn.com', 'impostellina@msn.com']
+
+let user = prompt('inserisci la tua email');
+//console.log(user)
+
+let flag = false
+
+for (let i=0; i < mails.length; i++){
+//console.log(mails[i])
+if( mails[i] === user){
+    flag = true
+}
+}
+
+if(flag === true){
+    //console.log('email presente')
+    document.getElementById('user').innerHTML=`la tua email ${user} è presente nella lista`
+} else {
+    //console.log('email non presente')
+    document.getElementById('user').innerHTML=`${user} non è tra le email registrate`
+}
