ff68f84c09c7dcb1857ff32d63f4604ceaf5a073
AntonelloStefania <antonelloste94@gmail.com>
2023-05-22 22:29:31 +0200

    corretto errore sintassi

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,15 +7,15 @@ let flag = false
 
 for (let i=0; i < mails.length; i++){
 //console.log(mails[i])
-if( mails[i].toLowerCase === user.toLowerCase){
+if( mails[i].toLowerCase() === user.toLowerCase()){
     flag = true
 }
 }
 
 if(flag === true){
-    //console.log('email presente')
+    console.log('email presente')
     document.getElementById('user').innerHTML=`la tua email ${user} è presente nella lista`
-} else {
-    //console.log('email non presente')
+} else{
+    console.log('email non presente')
     document.getElementById('user').innerHTML=`${user} non è tra le email registrate`
 }
